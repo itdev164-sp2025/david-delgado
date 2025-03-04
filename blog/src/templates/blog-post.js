1f801e0a307570678a30881dc7245a5cb85eb269
David Delgado <delgaddi@gmatc.matc.edu>
2025-03-03 18:50:51 -0600

    part 3 activity 3

--- blog/src/templates/blog-post.js
+++ blog/src/templates/blog-post.js
@@ -3,11 +3,14 @@ import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 
 const BlogPost = ({ data }) => {
-  const { title } = data.contentfulBlogPost
+  const { title, body } = data.contentfulBlogPost
 
   return (
     <Layout>
       <h1>{title}</h1>
+      <div
+        dangerouslySetInnerHTML={{ __html: body.childMarkdownRemark.html }}
+      ></div>
     </Layout>
   )
 }
@@ -16,9 +19,14 @@ export default BlogPost
 
 export const pageQuery = graphql`
   query blogPostQuery($slug: String!) {
-    contentfulBlogPost(slug: {eq: $slug}) {
+    contentfulBlogPost(slug: { eq: $slug }) {
       title
       slug
+      body {
+        childMarkdownRemark {
+          html
+        }
+      }
     }
   }
 `

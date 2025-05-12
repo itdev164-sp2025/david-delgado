33dbfe9d0a8025e16e5616acd25e3d8d0580d610
David Delgado <delgaddi@gmatc.matc.edu>
2025-05-12 05:29:20 -0500

    Completed Activity 7

--- blog/src/components/BaseContainer/BaseContainer.js
+++ blog/src/components/BaseContainer/BaseContainer.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Flex, Box } from 'rebass';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Flex, Box } from 'rebass'
 
 export const BaseContainer = ({ children, flex = false, ...rest }) => {
-  const Component = flex ? Flex : Box;
-  return <Component {...rest}>{children}</Component>;
-};
+  let Component = flex ? Flex : Box
+  return <Component {...rest}>{children}</Component>
+}
 
 BaseContainer.propTypes = {
   flex: PropTypes.bool,
   as: PropTypes.string,
-  children: PropTypes.isRequired,
-};
-
+  children: PropTypes.isRequired
+}
--- blog/src/templates/blog-post.js
+++ blog/src/templates/blog-post.js
@@ -1,33 +1,15 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout'
+import { H1 } from '../components/Heading'
 
 const BlogPost = ({ data }) => {
   const { title, body } = data.contentfulBlogPost
 
   return (
     <Layout>
-      <h1>{title}</h1>
-      <div
-        dangerouslySetInnerHTML={{ __html: body.childMarkdownRemark.html }}
-      ></div>
+      <H1>{title}</H1>
+      <div dangerouslySetInnerHTML={{__html: body.childMarkdownRemark.html}}></div>
     </Layout>
   )
 }
-
-export default BlogPost
-//bruh
-
-export const pageQuery = graphql`
-  query blogPostQuery($slug: String!) {
-    contentfulBlogPost(slug: { eq: $slug }) {
-      title
-      slug
-      body {
-        childMarkdownRemark {
-          html
-        }
-      }
-    }
-  }
-`
